9335c0ab7abcdfdbd7882d9af4515443321f6dcd
Muhammed <faraanshaik19@gmail.com>
2025-05-03 23:41:12 +0530

    app commit

--- src/App.jsx
+++ src/App.jsx
@@ -33,6 +33,7 @@ const App = () => {
         />
         <Route path="/login" element={<Login setToken={setToken} />} />
         <Route path="/register" element={<Register />} />
+        <Route path="/dashboard" element={<Dashboard />} />
 
         <Route
           path="/edit/:id"
